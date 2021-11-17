22229021df2e6733624c30cd86d4532c2267dc42
demowebsites <94529253+demowebsites@users.noreply.github.com>
2021-11-17 15:47:20 +0530

    added  js files

---
+++ JS/speakers.js
@@ -0,0 +1,10 @@
+const btnscrolltotop=document.querySelector('#btnscrolltotop');
+
+btnscrolltotop.addEventListener('click',function(){
+    window.scrollTo({
+          top:0,
+          left:0,
+          behavior:"smooth"
+
+    });
+})
